648b3ccf9cebeecd01e7e2e00a68663a0763a75a
François <francoislancastre@gmail.com>
2023-06-06 18:52:27 +0200

    controllers / routes / models / middlewara

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const mongoose = require("mongoose");
+const path = require("path");
+
+const booksRoutes = require("./routes/books");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -25,5 +31,68 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+// app.use((req, res, next) => {
+//   console.log("Requête reçue !");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.status(201);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.json({ message: "Votre requête a bien été reçue !" });
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Réponse envoyée avec succès !");
+// });
+
+// app.get("/api/books", (req, res, next) => {
+//   const books = [
+//     {
+//       userId: "dfeevef",
+//       title: "Voiture",
+//       author: "toto",
+//       imageUrl:
+//         "https://unsplash.com/photos/p7tai9P7H-s/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8dm9pdHVyZXxmcnwwfHx8fDE2ODU5NTgxODR8MA&force=true",
+//       year: 2010,
+//       genre: "roman",
+//       ratings: [
+//         {
+//           userId: "fbebe",
+//           grade: 5,
+//         },
+//       ],
+//       averageRating: 5,
+//     },
+//     {
+//       userId: "nrssrevef",
+//       title: "Moto",
+//       author: "titi",
+//       imageUrl:
+//         "https://unsplash.com/photos/-5rcxih1e44/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8bW90b3xmcnwwfHx8fDE2ODU5MDY1OTV8MA&force=true",
+//       year: 2000,
+//       genre: "Police",
+//       ratings: [
+//         {
+//           userId: "fdgbsbe",
+//           grade: 5,
+//         },
+//       ],
+//       averageRating: 5,
+//     },
+//   ];
+//   res.status(200).json(books);
+// });
+
+app.use(bodyParser.json());
+
+app.use("/api/books", booksRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
---
+++ backend/routes/books.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("../middleware/multer-config");
+
+const auth = require("../middleware/auth");
+
+const booksCtrl = require("../controllers/books");
+
+router.get("/", booksCtrl.getAllBook);
+router.post("/", auth, multer, booksCtrl.createBook);
+router.get("/bestrating", booksCtrl.getBestRatingBook);
+router.get("/:id", booksCtrl.getOneBook);
+router.put("/:id", auth, multer, booksCtrl.modifyBook);
+router.delete("/:id", auth, booksCtrl.deleteBook);
+
+module.exports = router;
